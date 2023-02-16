472b7d9b038f5de7f746c348c7fa65afac4a8b47
Sakshi-Goyat <bcs_2021059@iiitm.ac.in>
2023-02-17 03:15:21 +0530

    history is updated

--- server/controllers/history.js
+++ server/controllers/history.js
@@ -4,9 +4,9 @@ module.exports = async (req, res) => {
     console.log("h");
   try {
     console.log('p1');
-    const restore = await Transaction.find({ email: req.authuser.email });
+    console.log(req.authuser.email);
+    const restore = await Transaction.find({ senderEmail: req.authuser.email });
     res.send(restore);
-    console.log(restore);
   } catch (err) {
     console.log(err);
   }
